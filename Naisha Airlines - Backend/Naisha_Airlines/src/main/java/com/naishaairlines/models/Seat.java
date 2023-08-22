b075db5de979345123df7318dada83755c574e86
nmohammednawaz <nmohammednawaz786@gmail.com>
2023-08-22 21:33:38 +0530

    Ticket and Seat Model Created

--- Naisha Airlines - Backend/Naisha_Airlines/src/main/java/com/naishaairlines/models/Seat.java
+++ Naisha Airlines - Backend/Naisha_Airlines/src/main/java/com/naishaairlines/models/Seat.java
@@ -1,5 +1,50 @@
 package com.naishaairlines.models;
 
-public class Seat {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@NoArgsConstructor
+@Data
+public class Seat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotBlank(message = "Seat number is required")
+    private Integer seatNumber;
+    
+    @NotBlank(message = "Class type is required")
+    private String classType;
+    
+    @PositiveOrZero(message = "Price must be a positive or zero value")
+    private double price;
+    
+    @ManyToOne
+    @JoinColumn(name = "flight_number")
+    @NotNull(message = "Flight is required")
+    private Flight flight;
+    
+    @OneToOne(mappedBy = "seat")
+    private Booking booking;
+
+	public Seat(@NotBlank(message = "Class type is required") String classType,
+			@PositiveOrZero(message = "Price must be a positive or zero value") double price,
+			@NotNull(message = "Flight is required") Flight flight, Booking booking) {
+		super();
+		this.classType = classType;
+		this.price = price;
+		this.flight = flight;
+		this.booking = booking;
+	}
+    
+    
 }
--- Naisha Airlines - Backend/Naisha_Airlines/src/main/java/com/naishaairlines/models/Ticket.java
+++ Naisha Airlines - Backend/Naisha_Airlines/src/main/java/com/naishaairlines/models/Ticket.java
@@ -1,5 +1,69 @@
 package com.naishaairlines.models;
 
-public class Ticket {
+import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Ticket {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer ticketId;
+    
+    @NotBlank(message = "Ticket number is required")
+    private String ticketNumber;
+    
+    @ManyToOne
+    @JoinColumn(name = "passenger_id")
+    @NotNull(message = "Passenger is required")
+    private Passenger passenger;
+    
+    @ManyToOne
+    @JoinColumn(name = "flight_id")
+    @NotNull(message = "Flight is required")
+    private Flight flight;
+    
+    @ManyToOne
+    @JoinColumn(name = "seat_number")
+    @NotNull(message = "Seat is required")
+    private Seat seat;
+    
+    @OneToOne
+    @NotBlank(message = "Booking Id is required")
+    @JoinColumn(name = "booking_id")
+    private Booking booking;
+    
+    @PastOrPresent(message = "Booking date must be in the past or present")
+    private LocalDate bookingDate;
+
+	public Ticket(@NotBlank(message = "Ticket number is required") String ticketNumber,
+			@NotNull(message = "Passenger is required") Passenger passenger,
+			@NotNull(message = "Flight is required") Flight flight, @NotNull(message = "Seat is required") Seat seat,
+			@NotBlank(message = "Booking Id is required") Booking booking,
+			@PastOrPresent(message = "Booking date must be in the past or present") LocalDate bookingDate) {
+		super();
+		this.ticketNumber = ticketNumber;
+		this.passenger = passenger;
+		this.flight = flight;
+		this.seat = seat;
+		this.booking = booking;
+		this.bookingDate = bookingDate;
+	}
+    
+    
+    
 }
