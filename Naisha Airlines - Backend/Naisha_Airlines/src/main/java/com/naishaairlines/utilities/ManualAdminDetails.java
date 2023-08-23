374b91e22dc4489b9e45512e09ef3e54a3a81edd
nmohammednawaz <nmohammednawaz786@gmail.com>
2023-08-23 16:07:08 +0530

    Naisha Airlines

---
+++ Naisha Airlines - Backend/Naisha_Airlines/src/main/java/com/naishaairlines/utilities/ManualAdminDetails.java
@@ -0,0 +1,66 @@
+package com.naishaairlines.utilities;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.naishaairlines.models.Admin;
+
+public class ManualAdminDetails implements UserDetails {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Admin admin;
+
+	public ManualAdminDetails(Admin admin) {
+		super();
+		this.admin = admin;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return new ArrayList<>();
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return admin.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return admin.getUsername();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return admin.isActive();
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return admin.isActive();
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return admin.isActive();
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
---
+++ Naisha Airlines - Backend/Naisha_Airlines/src/main/java/com/naishaairlines/utilities/ManualPassengerDetails.java
@@ -0,0 +1,67 @@
+package com.naishaairlines.utilities;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.naishaairlines.models.Passenger;
+
+public class ManualPassengerDetails implements UserDetails {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Passenger passenger;
+
+	public ManualPassengerDetails(Passenger passenger) {
+		super();
+		this.passenger = passenger;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return new ArrayList<>();
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return passenger.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return passenger.getUsername();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return passenger.isActive();
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return passenger.isActive();
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return passenger.isActive();
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
